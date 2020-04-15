60b06021c05b1c9fbd4abf21efd5f4c4c0f6ddef
rugbier <rugbier@hotmail.com>
2020-04-15 16:22:18 +0200

    Refactor using lombook

--- EmailService/src/main/java/com/example/spring/cloud/streams/emailservice/consumer/Consumer.java
+++ EmailService/src/main/java/com/example/spring/cloud/streams/emailservice/consumer/Consumer.java
@@ -3,6 +3,7 @@ package com.example.spring.cloud.streams.emailservice.consumer;
 import java.util.Date;
 
 import com.example.spring.cloud.streams.transportdto.Notification;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -11,13 +12,11 @@ import org.springframework.messaging.handler.annotation.Payload;
 
 
 @EnableBinding(EmailSource.class)
+@Slf4j
 public class Consumer {
-
-	private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
-
 	@StreamListener(target = EmailSource.EMAIL, condition = "headers['type']=='email'")
 	public void handle(@Payload Notification notification) {
-		logger.info("Recieved notification message: [{}]: {}", new Date(), notification.toString());
+		log.info("Recieved notification message: [{}]: {}", new Date(), notification.toString());
 	}
 
 }
--- OrderService/src/main/java/com/example/spring/cloud/streams/orderservice/producer/MessageProducer.java
+++ OrderService/src/main/java/com/example/spring/cloud/streams/orderservice/producer/MessageProducer.java
@@ -1,17 +1,12 @@
 package com.example.spring.cloud.streams.orderservice.producer;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @EnableBinding({NotificationSource.class})
 @Data
+@AllArgsConstructor
 public class MessageProducer {
-
     private NotificationSource notificationSource;
-
-    public MessageProducer(NotificationSource emailSource) {
-        super();
-        this.notificationSource = emailSource;
-    }
-
 }
